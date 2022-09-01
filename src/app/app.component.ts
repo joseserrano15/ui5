f5719cfc1d2a14c8570467142428c46df7d47f47
joseli95 <31681512+joseli95@users.noreply.github.com>
2022-09-01 10:02:53 -0500

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
 
+import '@ui5/webcomponents-fiori/dist/SideNavigation.js';
+import '@ui5/webcomponents-fiori/dist/SideNavigationItem.js';
+import '@ui5/webcomponents-fiori/dist/SideNavigationSubItem.js';
+import '@ui5/webcomponents-icons/dist/AllIcons.js';
+import '@ui5/webcomponents-icons-business-suite/dist/AllIcons.js';
+import '@ui5/webcomponents/dist/Button';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
